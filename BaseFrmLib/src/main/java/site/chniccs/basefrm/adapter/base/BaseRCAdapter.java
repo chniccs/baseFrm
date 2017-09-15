e041adc9e7ee3e079420993710ec9aef78d1cc51
chencs <chniccs@163.com>
2017-09-15 11:05:36 +0800

    优化了BaseAdapter,默认情况下如果传入的泛型数据来List的子集，就不需要去设置count及自行设置数据。如果需要设置，需要重载方法

--- BaseFrmLib/src/main/java/site/chniccs/basefrm/adapter/base/BaseRCAdapter.java
+++ BaseFrmLib/src/main/java/site/chniccs/basefrm/adapter/base/BaseRCAdapter.java
@@ -3,31 +3,31 @@ package site.chniccs.basefrm.adapter.base;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 import site.chniccs.basefrm.holder.base.BaseRCHolder;
 import site.chniccs.basefrm.listener.ItemClickListener;
 import site.chniccs.basefrm.listener.ItemLongClickListener;
 
 
-public abstract class BaseRCAdapter<T>
-        extends RecyclerView.Adapter<BaseRCHolder>
-{
+public abstract class BaseRCAdapter<T extends List<?>>
+        extends RecyclerView.Adapter<BaseRCHolder> {
     protected T mData;
     private ItemClickListener myItemClickListener;
     private ItemLongClickListener myItemLongClickListener;
 
-    public void addData(T paramT) {}
+    public void addData(T paramT) {
+    }
 
     public abstract BaseRCHolder getItemHolder(ViewGroup paramViewGroup, int paramInt);
 
-    public void onBindViewHolder(BaseRCHolder paramBaseRCHolder, int paramInt)
-    {
+    public void onBindViewHolder(BaseRCHolder paramBaseRCHolder, int paramInt) {
         setItemData(paramBaseRCHolder, paramInt);
         paramBaseRCHolder.bindViewHolder(paramInt);
 
     }
 
-    public BaseRCHolder onCreateViewHolder(ViewGroup paramViewGroup, int paramInt)
-    {
+    public BaseRCHolder onCreateViewHolder(ViewGroup paramViewGroup, int paramInt) {
         BaseRCHolder localBaseRCHolder = getItemHolder(paramViewGroup, paramInt);
         if ((this.myItemClickListener != null) && (localBaseRCHolder != null)) {
             localBaseRCHolder.setOnClickListener(this.myItemClickListener);
@@ -38,20 +38,24 @@ public abstract class BaseRCAdapter<T>
         return localBaseRCHolder;
     }
 
-    public void setData(T paramT)
-    {
+    public void setData(T paramT) {
         this.mData = paramT;
     }
 
-    protected abstract void setItemData(BaseRCHolder paramBaseRCHolder, int paramInt);
+    protected void setItemData(BaseRCHolder paramBaseRCHolder, int paramInt) {
+        paramBaseRCHolder.setData(mData.get(paramInt));
+    }
 
-    public void setOnItemClickListener(ItemClickListener paramItemClickListener)
-    {
+    @Override
+    public int getItemCount() {
+        return mData == null ? 0 : mData.size();
+    }
+
+    public void setOnItemClickListener(ItemClickListener paramItemClickListener) {
         this.myItemClickListener = paramItemClickListener;
     }
 
-    public void setOnItemLongClickListener(ItemLongClickListener paramItemLongClickListener)
-    {
+    public void setOnItemLongClickListener(ItemLongClickListener paramItemLongClickListener) {
         this.myItemLongClickListener = paramItemLongClickListener;
     }
 }
